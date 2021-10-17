e64d6851bef6bf371fc4566ddfb2dfc8ca6b6626
Hubert Chen <hubertcy@live.com>
2021-10-17 00:13:15 -0500

    Add logging

--- Program.cs
+++ Program.cs
@@ -14,9 +14,12 @@ namespace SingletonExample
 
             Console.WriteLine("DIHeater1 hashcode: {0}", newDIHeater1.GetHashCode());
             Console.WriteLine("DIHeater1 hashcode: {0}", newDIHeater2.GetHashCode());
+            if (newDIHeater1 == newDIHeater2)
+                Console.WriteLine("DIHeater objects are the same");
             Console.WriteLine("IRHeater1 hashcode: {0}", newIRHeater1.GetHashCode());
             Console.WriteLine("IRHeater2 hashcode: {0}", newIRHeater2.GetHashCode());
-            
+            if (newIRHeater1 == newIRHeater2)
+                Console.WriteLine("IRHeater objects are the same");
         }
     }
 }
